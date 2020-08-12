80837f3ed944d7f04a9c580c80cfbaf13f80d250
chaser8 <chaser8@163.com>
2020-08-12 16:57:15 +0800

    new

---
+++ spring-boot-starter-tk-mybatis/src/main/java/top/chaser/framework/boot/starter/tkmybatis/mapper/BaseMapper.java
@@ -0,0 +1,12 @@
+package top.chaser.framework.boot.starter.tkmybatis.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @program:
+ * @description:
+ * @create: 2019-08-08 10:13
+ **/
+public interface BaseMapper<T> extends Mapper<T> {
+
+}
